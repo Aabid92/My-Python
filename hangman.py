8f6f97837e8a44e6c44c1ee107c1a40384ff44a4
Aabid92 <shaikhaabid608@gmail.com>
2023-03-11 13:27:32 +0530

    adding more codes

--- hangman.py
+++ hangman.py
@@ -15,12 +15,18 @@ for _ in range(word_len):
     display += "_"
 print(display)     
 
-guess = input("guess a latter: ").lower()
+end_of_game = False
+while not end_of_game :
+    guess = input("guess a latter: ").lower()
 
-for position in range(word_len):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-print(display)
+    for position in range(word_len):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("your win")
 
 
